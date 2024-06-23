c6f8c9adda42bd2bd6d83497b474e681de8ae22f
Jep <jacob.dyke30@gmail.com>
2024-06-23 16:49:24 -0400

    Quick updates

--- src/app/contact/submit/page.tsx
+++ src/app/contact/submit/page.tsx
@@ -8,7 +8,7 @@ export default function submitPage() {
     return (<>
         <Navbar />
         <div className="h-[75vh] flex flex-col justify-center items-center gap-3">
-            <h1 className="text-xl">Thank you for your submission. Please allow us 1-2 business days to respond!</h1>
+            <h1 className="text-xl text-center">Thank you for your submission. Please allow us 1-2 business days to respond!</h1>
             <a href="/" className="border-2 border-slate-900 rounded-md px-5 py-2 flex flex-row items-center justify-center gap-2 hover:-translate-y-2 transition-all duration-150">Return <FaArrowTurnUp /></a>
         </div>
         <Footer />
--- src/app/modules/servicepage/ServicePageHero.tsx
+++ src/app/modules/servicepage/ServicePageHero.tsx
@@ -1,20 +1,20 @@
 import Hammer from "@/app/media/hammer.jpg"
 
-export default function ServicePageHero(){
+export default function ServicePageHero() {
 
-    return(<>
+    return (<>
         <div className="h-[45vh] flex flex-col justify-center items-center relative" style={{
-            backgroundImage:`url(${Hammer.src})`,
-            backgroundPositionX:'-150px',
-            backgroundPositionY:'-300px',
-            backgroundSize:'135%'
+            backgroundImage: `url(${Hammer.src})`,
+            backgroundPositionX: '-150px',
+            backgroundPositionY: '-300px',
+            backgroundSize: '135%'
         }}>
-            <div className="h-[45vh] w-full bg-slate-500 absolute z-20 opacity-50"/>
-            <h1 
-            className="text-center text-7xl p-14 z-30 text-white font-bold"
-            style={{
-                textShadow:"0px 5px 15px gray"
-            }}
+            <div className="h-[45vh] w-full bg-slate-500 absolute z-20 opacity-50" />
+            <h1
+                className="text-center sm:text-7xl text-5xl p-14 z-30 text-white font-bold"
+                style={{
+                    textShadow: "0px 5px 15px gray"
+                }}
             >
                 What We Do
             </h1>
